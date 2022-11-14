6ad476f33d5de30795329162131e28c673201683
Andrew Finsand <andfinsand@gmail.com>
2022-11-13 17:25:21 -0800

    Npm tailwind install and configuration. In jstoolchain folder, run "npm run tailwind-watch". CD back to root folder and run server. Any changes made to the tailwindconfig file or "input.css" file will update "style.css" file. "style.css" is added to .gitignore.

---
+++ jstoolchain/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../templates/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'yellow-test': '#FFD333',
+      },
+    },
+  },
+  plugins: [],
+}
