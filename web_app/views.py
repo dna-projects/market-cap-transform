5e7f26446e5caa043908e019ca06c1cbf58ead45
colord <10601293+colord@users.noreply.github.com>
2022-11-18 10:17:00 -0800

    Update Baseview to InitialPageView. Add post method. Add TransformPageView. Add token class to keep track of properties. Pass token data into context.

--- web_app/views.py
+++ web_app/views.py
@@ -1,9 +1,38 @@
+from dataclasses import dataclass
 from django.views.generic import TemplateView
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-class BaseView(TemplateView):
+class InitialPageView(TemplateView):
     template_name = "initial.html"
 
+    # TODO - Perform api call from inside Django or use Javascript?
+    #      - 
+
     def get(self, request):
-        print('🐍 Hello from the Django backend! 🐍')
         return render(request, self.template_name)
+
+    # TODO - Pass data (token a, token b) into TransformPageView
+    # TODO - Check if redirect works (first add form to template file)
+    def post(self, request):
+        return redirect('transform')
+
+class TransformPageView(TemplateView):
+    template_name = "transform.html"
+
+    # Keep track of token properties
+    @dataclass
+    class Token:
+        name: str
+        img: str = None
+        price: float = None
+        percentage: float = None
+
+    token_a = Token('Evmos', '💸', 451.23, 297)
+
+    # TODO - Figure out how to handle what happens when the name 
+    # spans onto a separate line. 
+    token_b = Token('Cryptocurrency with super long name...')
+
+    def get(self, request):
+        return render(request, self.template_name, 
+            {"token_a": self.token_a, "token_b": self.token_b})
