798b8e2100e94083043245770ff0077267cdceab
colord <10601293+colord@users.noreply.github.com>
2022-11-18 13:28:32 -0800

    Add function to make a covalent api request. Uses COVALENT_API_KEY in dotenv file

---
+++ web_app/services.py
@@ -0,0 +1,20 @@
+import requests
+from requests.exceptions import Timeout, HTTPError
+from decouple import config
+
+def get_covalent_data(token_id, endpoint):
+    base_url = 'https://api.covalenthq.com/v1'
+    api_key = config('COVALENT_API_KEY')
+    complete_url = f'{base_url}/{token_id}/{endpoint}'
+    parameters = {'key': api_key}
+
+    # Attempt to get data and handle errors along the way
+    try:
+        response = requests.get(complete_url, params=parameters, timeout=10)
+        response.raise_for_status()
+
+        return response.json()
+    except Timeout as err:
+        print("Request timed out:", err)
+    except HTTPError as err:
+        print("Http error:", err)
