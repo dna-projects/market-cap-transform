e0835a476bd12fa4dd81f4c68849672712fdbed2
colord <10601293+colord@users.noreply.github.com>
2022-11-22 10:36:35 -0800

    Allow InitialPageView to handle get requests (json formatted) from frontend JS request

--- web_app/views.py
+++ web_app/views.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
+from django.http import JsonResponse
 from django.views.generic import TemplateView, FormView
 from django.shortcuts import render, redirect
 from web_app.services import get_covalent_data, get_coingecko_data
 from web_app.forms import TokenFormA, TokenFormB
+import json
 
 class InitialPageView(FormView):
     template_name = "initial.html"
@@ -10,15 +12,26 @@ class InitialPageView(FormView):
     form_B = TokenFormB
 
     def get(self, request):
-        # UNCOMMENT TO make covalent API request 
-        # NOTE - Must add COVALENT_API_KEY='...api key...' to .env file
+        if request.accepts('text/html'):
+            # UNCOMMENT TO make covalent API request 
+            # NOTE - Must add COVALENT_API_KEY='...api key...' to .env file
 
-        # tokens_data = get_covalent_data('9001', 'xy=k/diffusion/tokens/')
-        # token_list = [token['contract_ticker_symbol'] for token in tokens_data['data']['items']]
-        # print(token_list)
-        search = get_coingecko_data("evmos")
-        print(search["coins"][0]["id"])
-        return render(request, self.template_name, {"form_A":self.form_A , "form_B":self.form_B})
+            # tokens_data = get_covalent_data('9001', 'xy=k/diffusion/tokens/')
+            # token_list = [token['contract_ticker_symbol'] for token in tokens_data['data']['items']]
+            # print(token_list)
+
+            # NOTE - Uncomment to make coingecko api request
+            # search = get_coingecko_data("evmos")
+            # print(search["coins"][0]["id"])
+            return render(request, self.template_name, {"form_A":self.form_A , "form_B":self.form_B})
+        else:
+            # When the JS from the frontend makes a post request, Django
+            # will return data using JsonResponse 
+
+            # data_from_post = json.load(request)['token_query']
+            # Django prefers the data given to the frontend is a dictionary
+            data = {'test': 'a test!'}
+            return JsonResponse(data)
 
     # TODO - Pass data (token a, token b) into TransformPageView
     # TODO - Check if redirect works (first add form to template file)
