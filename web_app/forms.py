05cbe7535557ffa30204f33f7bea9c30047a7a19
Andrew Finsand <andfinsand@gmail.com>
2022-12-01 20:06:33 -0800

    Dropdown stylized with token list from API.

--- web_app/forms.py
+++ web_app/forms.py
@@ -2,30 +2,24 @@ from django.forms import Form, CharField, TextInput
 
 # Token A input field
 
-class TokenFormA(Form):
+class TokenForm(Form):
     token_input_A = CharField(
         max_length = 35,
         widget=TextInput(attrs={
-            'placeholder' : 'Enter token A...',
+            'placeholder' : 'Add Evmos token...',
         })
     )
 
-    def __init__(self, *args, **kwargs):
-        super(TokenFormA, self).__init__(*args, **kwargs)
-        self.fields['token_input_A'].widget.attrs.update({'class': 'token-input'})
-        self.fields['token_input_A'].label = '' # label = '' is required to remove a label above the input field which pushes the input field down.
-
-# Token B input field
-
-class TokenFormB(Form):
     token_input_B = CharField(
         max_length = 35,
         widget=TextInput(attrs={
-            'placeholder' : 'Enter token B...',
+            'placeholder' : 'Add any token...',
         })
     )
 
     def __init__(self, *args, **kwargs):
-        super(TokenFormB, self).__init__(*args, **kwargs)
+        super(TokenForm, self).__init__(*args, **kwargs)
+        self.fields['token_input_A'].widget.attrs.update({'class': 'token-input'})
+        self.fields['token_input_A'].label = '' # label = '' is required to remove a label above the input field which pushes the input field down.
         self.fields['token_input_B'].widget.attrs.update({'class': 'token-input'})
         self.fields['token_input_B'].label = '' # label = '' is required to remove a label above the input field which pushes the input field down.
